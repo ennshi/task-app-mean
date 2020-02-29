78a534082320ce087b09375ed2f34368cab64df9
ennshi <enn.shi@gmail.com>
2020-03-01 00:11:55 +0100

    menu collapse added

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 
@@ -8,18 +8,30 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
-
+  menuCollapsed: boolean;
+  innerWidth: number;
+  mobileMenu: boolean;
   constructor(private router: Router,
               public auth: AuthService) { }
 
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    this.collapseMenu();
   }
-
   logout(event: Event) {
     event.preventDefault();
     this.auth.logoutOnClick().subscribe(() => {
       this.auth.logout();
       this.router.navigate(['/', 'login']);
     });
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    this.collapseMenu();
+  }
+  collapseMenu() {
+    this.menuCollapsed = this.innerWidth <= 600;
+    this.mobileMenu = this.innerWidth <= 600;
   }
 }
