3cb997de737fdda9a2a4c9865e75858ade3ea871
ennshi <enn.shi@gmail.com>
2020-03-02 00:50:46 +0100

    user schema fixed

--- server/models/user.js
+++ server/models/user.js
@@ -21,15 +21,6 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be positive');
-            }
-        }
-    },
     password: {
         type: String,
         minlength: 7,
