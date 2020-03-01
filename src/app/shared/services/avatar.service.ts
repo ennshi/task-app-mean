43328b78640cd8d33eef8c95a707d2621346e5d4
ennshi <enn.shi@gmail.com>
2020-03-01 01:02:25 +0100

    avatar errorhandler added

--- src/app/shared/services/avatar.service.ts
+++ src/app/shared/services/avatar.service.ts
@@ -1,15 +1,26 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Subject, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class AvatarService {
   public fallbackUrl = '/assets/images.jpg';
+  public errorAvatar$: Subject<string> = new Subject<string>();
   constructor(private http: HttpClient) {}
-  delete():Observable<any> {
+  delete() {
     return this.http.delete('/api/users/me/avatar');
   }
-  add(image):Observable<any> {
-    return this.http.post('/api/users/me/avatar', image);
+  add(image: any) {
+    return this.http.post('/api/users/me/avatar', image)
+      .pipe(
+        catchError(this.handleError.bind(this))
+      );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 400) {
+      this.errorAvatar$.next('Please add an avatar (jpg, jpeg or png) and smaller than 1MB');
+    }
+    return throwError(error);
   }
 }
